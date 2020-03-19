7b723ee2865292a1ea5df3d5c74ce33ab7ad51ba
chendewei <cdw75901023@163.com>
2020-03-19 18:24:19 +0800

    test

--- luan/2.12.c
+++ luan/2.12.c
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 #include <math.h> 
 
-int main()
+
+int main() 
 {
+	int i=0;
+	int count =0;
+	for (i=100;i<=200;i++)
+	{ 
+		int j=0;
+		for(j=2;j<i;j++)
+		{
+			if (i%j==0)
+			{
+				break;
+			}
+		}
+		if(i==j)
+		{
+			printf("%d ",i);
+			count++;
+		}
+	}
+	printf("\ncount=%d\n",count);
 	
 }
 
